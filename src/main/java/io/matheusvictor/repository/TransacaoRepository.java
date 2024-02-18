adb174d7af5e24f7f7a70fffe0a97abb38526bbf
MatheusVict <matheusvictorhenrique@gmail.com>
2024-02-18 00:43:27 -0300

    feat: get the last 10 transaction

--- src/main/java/io/matheusvictor/repository/TransacaoRepository.java
+++ src/main/java/io/matheusvictor/repository/TransacaoRepository.java
@@ -2,8 +2,16 @@ package io.matheusvictor.repository;
 
 import io.matheusvictor.entity.Transacao;
 import io.quarkus.hibernate.reactive.panache.PanacheRepository;
+import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.List;
 
 @ApplicationScoped
 public class TransacaoRepository implements PanacheRepository<Transacao> {
+    public Uni<List<Transacao>> find10LastTransactionsOrderedByDate(Long clienteId) {
+        return find("clienteId = ?1 order by efetuada_em desc LIMIT 10", clienteId)
+
+                .list();
+    }
 }
