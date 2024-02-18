0a3798a558fad1ef565de5498859ea12639caf86
MatheusVict <matheusvictorhenrique@gmail.com>
2024-02-17 23:03:13 -0300

    feat: intial commit
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ src/test/java/io/matheusvictor/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package io.matheusvictor;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
